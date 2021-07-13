9370293c7de9edf9fc1c87dfa154f6fecd411a3a
osher <oshervatari@gmail.com>
2021-07-13 20:23:11 +0300

    photo upload in defined ratio

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './quasar'
 import firebaseInstance from './middleware/firebase'
+import Croppa from 'vue-croppa';
+Vue.use(Croppa);
 
 Vue.config.productionTip = false
 firebaseInstance.firebase.auth().onAuthStateChanged((user )=>{
